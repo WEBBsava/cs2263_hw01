543502d3034d43e17520f86a9d9d8df9ee6ebde4
Savannah Webb <webbsav2@isu.edu>
2022-02-06 19:54:37 -0700

    Fixed expression evaluator problem

--- app/src/main/java/edu/isu/cs2263/hw01/output.java
+++ app/src/main/java/edu/isu/cs2263/hw01/output.java
@@ -1,57 +1,16 @@
 package edu.isu.cs2263.hw01;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.Scanner;
-
 /**
- *
+ * A simple interface that formats the output.
  * @author Savannah Webb
  */
 
-public interface input {
-    static void output(String result) {
+public interface output {
+    /**
+     *
+     * @param result
+     */
+    static void out(String result) {
         System.out.printf("  -> %s\n", result);
     }
-
-    /**
-     * Simple method that handles reading a given batch file and returning the answers.
-     * @param batchFile
-     */
-
-    /**
-     * Scanner scanner;
-     * scanner = new Scanner(new File("path"));
-     *
-     * while(scanner.hasNext()) {
-     *   String str = scanner.nextLine();
-     *   // Do something with line
-     * }
-     *
-     * String filename = "./thisfile.txt";
-     * Scanner scan = new Scanner(filename);
-     * System.out.printLn(scan.nextLine());
-     */
-    static void batch_input(Path batchFile) {
-        Scanner scanner;
-        String file = "/" + String.valueOf(batchFile);
-        try {
-            scanner = new Scanner(new File(file));
-            while(scanner.hasNext()) {
-                String expr = scanner.nextLine();
-                int result = expression_evaluator.eval_expression(expr);
-                String out = expr + Integer.toString(result);
-                output(out);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-
-
-    }
-
 }
